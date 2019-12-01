a3c706563cf1f1af11264067711ea32ba148b306
ioanaMMT <ioana2morosanu@gmail.com>
2019-12-01 15:16:59 +0000

    Add new entry

--- course_det.py
+++ course_det.py
@@ -1,6 +1,6 @@
 from course import *
 
-course = Course("Introduction", 100, 8, "python")
+course = Course("Introduction", 100, 8, "python", 17)
 
 print course.name
 print course.course_price()
@@ -8,3 +8,5 @@ print course.course_price()
 print(course.name + " " + course.course_type + " " + str(course.course_price()))
 
 print course.get_passing_grade()
+
+print course.price_for_kids()
